0837770259720ffd15598bc3dc37060a88d199aa
Moiz Akbar <99669312+moiz284@users.noreply.github.com>
2025-01-27 01:20:54 +0500

    Update moizllm.py

--- moizllm.py
+++ moizllm.py
@@ -11,8 +11,12 @@ import os
 import gdown
 
 # Download the file from Google Drive using its shareable link
-gdown.download("https://drive.google.com/drive/folders/1C6ImLPbM7i17vKZ4iqUw3SMjOz9_mhHC?usp=sharing", "moiz", quiet=False)
-
+-gdown.download(
+    "https://drive.google.com/drive/folders/1C6ImLPbM7i17vKZ4iqUw3SMjOz9_mhHC?usp=sharing",
+    "moiz",
+    fuzzy=True,
+    quiet=False
+)
 
 import pandas as pd
 import os
